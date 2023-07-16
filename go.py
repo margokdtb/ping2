acd118b945e94fc5429ce35520ff2fb8fb928866
Margo <57738351+margokdtb@users.noreply.github.com>
2023-07-16 18:56:27 +0700

    Add files via upload

--- go.py
+++ go.py
@@ -10,10 +10,11 @@ def display_menu():
     print("Menu:")
     print("1. Cari Sub Domain (Koneksi Internet) ")
     print("2. Scan Direct Websoket (Koneksi paket data)")
-    print("3. Scan SSL CDN (Koneksi paket data) ")
-    print("4. Ping SSL CDN (Copas dl hasil No 2 ke input.txt)")
-    print("5. Scan SNI (Koneksi paket data) ")
-    print("6. Ping SNI (Koneksi paket data) ")
+    print("3. Ping Direct (Koneksi paket data)")
+    print("4. Scan SSL CDN (Koneksi paket data) ")
+    print("5. Ping SSL CDN (Copas dl hasil No 2 ke input.txt)")
+    print("6. Scan SNI (Koneksi paket data) ")
+    print("7. Ping SNI (Koneksi paket data) ")
     
     print("0. Keluar")
 
@@ -22,14 +23,15 @@ def run_file(file_number):
         os.system("python3 1.py")
     elif file_number == 2:
         os.system("python3 4.py")
-        
     elif file_number == 3:
-        os.system("python3 2.py") 
+        os.system("python3 ping_direct.py")  
     elif file_number == 4:
-        os.system("python3 3v1.py")
+        os.system("python3 2.py") 
     elif file_number == 5:
-        os.system("python3 sni1.py")
+        os.system("python3 3v1.py")
     elif file_number == 6:
+        os.system("python3 sni1.py")
+    elif file_number == 7:
         os.system("python3 sni2.py")
         
     elif file_number == 0:
@@ -39,7 +41,7 @@ def run_file(file_number):
 
 while True:
     display_menu()
-    choice = int(input("Masukkan pilihan (0-6): "))
+    choice = int(input("Masukkan pilihan (0-7): "))
     run_file(choice)
     if choice == 0:
         break

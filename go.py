50ed8d563558ed2d2dd7df444103cc4b1ac0211e
Margo <57738351+margokdtb@users.noreply.github.com>
2023-07-17 15:24:16 +0700

    Add files via upload
    Ringkas menu

--- go.py
+++ go.py
@@ -9,38 +9,30 @@ import os
 def display_menu():
     print("Menu:")
     print("1. Cari Sub Domain (Koneksi Internet) ")
-    print("2. Scan Direct Cloudflare (Koneksi paket data)")
-    print("3. Ping Direct ")
-    print("4. Scan Websoket ")
-    print("5. Scan SSL CDN ")
     
-    print("6. Scan SNI ")
-    print("7. Ping SNI ")
-    print("8. Scan CDN SSL v2  ")
-    print("9. Ping CDN SSL v2 (Copas dl hasil No 8 ke input.txt)")
+    print("2. Scan Websoket (Koneksi Data) ")
+    print("3. Scan SSL CDN (Koneksi Data) ")
+    
+    print("4. Scan SNI (Koneksi Data) ")
+    print("5. Ping SNI (Koneksi Data) ")
+    
+    
     
     print("0. Keluar")
 
 def run_file(file_number):
     if file_number == 1:
         os.system("python3 1.py")
-    elif file_number == 2:
-        os.system("python3 4.py")
-    elif file_number == 3:
-        os.system("python3 ping_direct.py")  
     
-    elif file_number == 4:
+    elif file_number == 2:
         os.system("python3 ws80.py")
-    elif file_number == 5:
+    elif file_number == 3:
         os.system("python3 cdnssl1.py") 
-    elif file_number == 6:
+    elif file_number == 4:
         os.system("python3 sni1.py")
-    elif file_number == 7:
+    elif file_number == 5:
         os.system("python3 sni2.py")
-    elif file_number == 8:
-        os.system("python3 2.py")
-    elif file_number == 9:
-        os.system("python3 3v1.py")
+
         
     elif file_number == 0:
         return
@@ -50,7 +42,7 @@ def run_file(file_number):
 while True:
     display_menu()
     try:
-        choice = int(input("Masukkan pilihan (0-9): "))
+        choice = int(input("Masukkan pilihan (0-5): "))
         run_file(choice)
         if choice == 0:
             break

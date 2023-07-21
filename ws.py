c5ec4c937c791f19203b604383681eae487d08ce
Margo <57738351+margokdtb@users.noreply.github.com>
2023-07-21 10:10:35 +0700

    Add files via upload
    Ganti sumber

--- ws.py
+++ ws.py
@@ -1,12 +1,14 @@
 print("\n\n SCAN CLOUDFLARE \n")
 
 import os
-os.system("bugscanner-go scan direct -f subdomain.txt -o hasil2_direct.txt" )
-#print("\n\nHasil telah disimpan di file hasil2_direct.txt\n")
+#os.system("bugscanner-go scan direct -f subdomain.txt -o hasil2_direct.txt" )
+print("\n\nHasil telah disimpan di file hasil2_direct.txt\n")
 
 #scan ws port 80
-import ws803
-import wsssl
+os.system("python ws803.py" )
+os.system("python wsssl.py" )
+#import ws80
+#import wsssl
 
 print("\n\n HASIL DISIMPAN : \n") 
 print(" - >> hasil_websocket80_ping.txt \n")

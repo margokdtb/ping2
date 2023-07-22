e8770c3f6779812f1372bc70ed8bb5977240ad6f
Margo <57738351+margokdtb@users.noreply.github.com>
2023-07-22 13:12:42 +0700

    Add files via upload

--- 1.py
+++ 1.py
@@ -6,5 +6,5 @@ domain = input("Masukan Domain: ")
 #os.system("cd")
 #os.system("cd storage/downloads/scanws/scanws")
 
-os.system(f"subfinder -d {domain} -o subdomain2.txt")
+os.system(f"subfinder -d {domain} -o subdomain.txt")
 print("\n\n  Data disimpan di subdomain.txt \n\n ")

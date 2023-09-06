6131b776336e928e82e262cb42fd76410183dff9
Margo <57738351+margokdtb@users.noreply.github.com>
2023-09-06 11:16:15 +0700

    Add files via upload

--- sumberlain.py
+++ sumberlain.py
@@ -1,35 +1,24 @@
-import re
+def extract_host_ip(data):
+    host_ip_list = []
+    lines = data.split('\n')
+    for line in lines:
+        line = line.strip()
+        if line:
+            if ' - ' in line:
+                host_ip = line.split(' - ')[-1].strip()
+                host_ip_list.append(host_ip)
+            elif '.' in line:
+                host_ip_list.append(line)
+    return host_ip_list
 
-print("\n\n Isi dulu file sumber_pcapdroid.txt \n\n")
+filename = "sumber/sumber_pcapdroid.txt"
+output_filename = "subdomain.txt"
 
-# Konfirmasi dari pengguna
-konfirmasi = input("sudah mengisi file? (y/n) ")
+with open(filename, "r", encoding="latin-1") as file:
+    data = file.read()
 
-if konfirmasi.lower() == 'y':
-    # Membaca file sumber_pcapdroid.txt dan mengambil subdomain dan ip
-    with open('sumber/sumber_pcapdroid.txt', 'r') as file:
-        data = file.read()
-        subdomains = re.findall(r'\b(?:[A-Za-z0-9]+\.)+[A-Za-z]{2,}\b', data)
-        ips = re.findall(r'\b(?:\d{1,3}\.){3}\d{1,3}\b', data)
-    
-    # Menggunakan set untuk membuat subdomain dan ip yang unik
-    unique_subdomains = set(subdomains)
-    unique_ips = set(ips)
+host_ip_list = extract_host_ip(data)
 
-    # Menyimpan subdomain dan ip yang unik dalam file hasil.txt
-    with open('subdomain.txt', 'w') as file:
-        #file.write("Subdomains:\n")
-        for subdomain in unique_subdomains:
-            file.write(subdomain + '\n')
-        
-        # file.write("\nIPs:\n")
-        for ip in unique_ips:
-            file.write(ip + '\n')
-            
-    # import os
-    # os.system("python unik3.py")
-
-    print('\n Data telah disimpan di hasil.txt')
-
-else:
-    print("\n Pastikan untuk mengisi file sumber_pcapdroid.txt sebelum menjalankan program ini.")
+with open(output_filename, "w") as file:
+    for host_ip in host_ip_list:
+        file.write(host_ip + "\n")
